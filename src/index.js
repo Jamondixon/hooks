03dd028798ab81fd65b2b946f5e9ed4dc6284ff4
Jamon Dixon <jamonadixon@gmail.com>
2021-08-06 12:22:48 -0500

    useEffect

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React,  { useState, useEffect }from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+function App() {
+  const [name, setName] = useState("Jan")
+  
+  useEffect(() => {
+    document.title = `Celebrate ${name}`
+  })
+  
+  return (
+    <section>
+      <p>Congratulations {name}!</p>
+      <button onClick={() => setName("Will")}>Change Winner</button>
+    </section>
+  )
+}
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,7 +26,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
