af91e6ce2fa835e8a9938dbdaff8e1f67d073ad3
Jamon Dixon <jamonadixon@gmail.com>
2021-08-19 16:06:18 -0500

    useRef

--- src/index.js
+++ src/index.js
@@ -1,34 +1,54 @@
-import React,  { useState, useEffect }from 'react';
+import React,  { useRef }from 'react';
 
 import ReactDOM from 'react-dom';
 import './index.css';
 
+// function App() {
+//   const [data, setData] = useState([])
 
-function App() {
-  const [data, setData] = useState([])
+  // useEffect(() => {
+  //   fetch("https://api.github.com/users")
+  //   .then(response => response.json())
+  //   .then(setData)
+  // }, [])
 
-  useEffect(() => {
-    fetch("https://api.github.com/users")
-    .then(response => response.json())
-    .then(setData)
-  }, [])
-
-  if(data) {
-    return (
-      <div>
-        <ul>
-          {data.map((user) => (
-            <li key={user.id}>{user.login}</li>
-          ))}
-        </ul>
-        <button onClick={() => setData()}>Remove Data</button>
-      </div>
-    )
-  }
+  // if(data) {
+  //   return (
+  //     <div>
+  //       <ul>
+  //         {data.map((user) => (
+  //           <li key={user.id}>{user.login}</li>
+  //         ))}
+  //       </ul>
+  //       <button onClick={() => setData()}>Remove Data</button>
+  //     </div>
+  //   )
+  // }
   
-  return (
-    <p>No Users</p>
-  )
+  // return (
+  //   <p>No Users</p>
+  // )
+// }
+function App(){
+  const sound = useRef()
+  const color = useRef()
+
+  const submit = (e) => {
+    e.preventDefault();
+    const soundValue = sound.current.value
+    const colorValue = color.current.value
+    alert(`${soundValue} sounds like ${colorValue}`)
+    sound.current.value = ""
+    color.current.value = ""
+  }
+
+    return (
+      <form onSubmit={submit}>
+        <input ref={sound} type="text" placeholder="sound..."/>
+        <input ref={color} type="color" />
+        <button>ADD</button>
+      </form>
+    )
 }
 
 
