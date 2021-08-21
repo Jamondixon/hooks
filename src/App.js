058d53837af1c50b2f9c893155ac919141f10c9e
Jamon Dixon <jamonadixon@gmail.com>
2021-08-20 21:44:25 -0500

    useContext

--- src/App.js
+++ src/App.js
@@ -1,1 +1,16 @@
+import { useContext } from "react"
+import {treesContext} from "./"
 
+function App() {
+    const {trees} = useContext(TreesContext)
+    return(
+        <div>
+            <h1>Trees Ive heard of</h1>
+            <ul>
+                {trees.map((tree) => (
+                <li key={tree.id}>{tree.type}</li>
+                ))}
+            </ul>
+        </div>
+    )
+}
